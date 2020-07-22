c6f1ba7dac94f8234fc3b85a07570746005059b7
Adam "Crashdoom" Walker <575410+Crashdoom@users.noreply.github.com>
2020-07-22 09:22:29 -0500

    add error handling, add psa, fix minor issues

--- api.js
+++ api.js
@@ -1,5 +1,3 @@
-// api.conqbladestats.com
-
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -13,26 +11,30 @@ const corsHeaders = {
 async function updateMyGames(request) {
   if (request.method !== 'POST') {
     return sendJSONResponse({ err: 'method not allowed' }, 405);
+  } 
+
+  try {
+    const headers = new Map(request.headers);
+    const authorization = headers.get('authorization');
+
+    if (!authorization || authorization !== 'Bearer ADD_BEARER_TOKEN_HERE') {
+      return sendJSONResponse({ err: 'access denied' }, 401);
+    }
+
+    const data = await request.json();
+    const date = new Date(data.last_updated);
+    const dateKey = `${date.getUTCFullYear()}-${leftPad(date.getUTCMonth())}-${leftPad(date.getUTCDate())}_${leftPad(date.getUTCHours())}:${leftPad(date.getUTCMinutes())}:${leftPad(date.getUTCSeconds())}`;
+
+    const previousStats = await STATS.get('myGames-latest');
+    addComparisonData(JSON.parse(previousStats), data);
+
+    await STATS.put('myGames-latest', JSON.stringify(data));
+    await STATS.put(`myGames-${dateKey}`, JSON.stringify(data));
+
+    return sendJSONResponse({ ok: true }, 200);
+  } catch (e) {
+    return sendJSONResponse({ ok: false, msg: e.toString() }, 503);
   }
-
-  const headers = new Map(request.headers);
-  const authorization = headers.get('authorization');
-
-  if (!authorization || authorization !== 'Bearer REDACTED') {
-    return sendJSONResponse({ err: 'access denied' }, 401);
-  }
-
-  const data = await request.json();
-  const date = new Date(data.last_updated);
-  const dateKey = `${date.getUTCFullYear()}-${leftPad(date.getUTCMonth())}-${leftPad(date.getUTCDate())}_${leftPad(date.getUTCHours())}:${leftPad(date.getUTCMinutes())}:${leftPad(date.getUTCSeconds())}`;
-
-  const previousStats = await STATS.get('myGames-latest');
-  addComparisonData(JSON.parse(previousStats), data);
-
-  await STATS.put('myGames-latest', JSON.stringify(data));
-  await STATS.put(`myGames-${dateKey}`, JSON.stringify(data));
-
-  return sendJSONResponse({ ok: true }, 200);
 }
 
 async function updateBoomingGames(request) {
@@ -40,24 +42,28 @@ async function updateBoomingGames(request) {
     return sendJSONResponse({ err: 'method not allowed' }, 405);
   }
 
-  const headers = new Map(request.headers);
-  const authorization = headers.get('authorization');
+  try {
+    const headers = new Map(request.headers);
+    const authorization = headers.get('authorization');
 
-  if (!authorization || authorization !== 'Bearer REDACTED') {
-    return sendJSONResponse({ err: 'access denied' }, 401);
+    if (!authorization || authorization !== 'Bearer ADD_BEARER_TOKEN_HERE') {
+      return sendJSONResponse({ err: 'access denied' }, 401);
+    }
+
+    const data = await request.json();
+    const date = new Date(data.last_updated);
+    const dateKey = `${date.getUTCFullYear()}-${leftPad(date.getUTCMonth())}-${leftPad(date.getUTCDate())}_${leftPad(date.getUTCHours())}:${leftPad(date.getUTCMinutes())}:${leftPad(date.getUTCSeconds())}`;
+
+    const previousStats = await STATS.get('boomingGames-latest');
+    addComparisonData(JSON.parse(previousStats), data);
+
+    await STATS.put('boomingGames-latest', JSON.stringify(data));
+    await STATS.put(`boomingGames-${dateKey}`, JSON.stringify(data));
+
+    return sendJSONResponse({ ok: true }, 200); 
+  } catch (e) {
+    return sendJSONResponse({ ok: false, msg: e.toString() }, 503);
   }
-
-  const data = await request.json();
-  const date = new Date(data.last_updated);
-  const dateKey = `${date.getUTCFullYear()}-${leftPad(date.getUTCMonth())}-${leftPad(date.getUTCDate())}_${leftPad(date.getUTCHours())}:${leftPad(date.getUTCMinutes())}:${leftPad(date.getUTCSeconds())}`;
-
-  const previousStats = await STATS.get('boomingGames-latest');
-  addComparisonData(JSON.parse(previousStats), data);
-
-  await STATS.put('boomingGames-latest', JSON.stringify(data));
-  await STATS.put(`boomingGames-${dateKey}`, JSON.stringify(data));
-
-  return sendJSONResponse({ ok: true }, 200);
 }
 
 /**
@@ -67,28 +73,36 @@ async function updateBoomingGames(request) {
 async function handleRequest(request) {
   const path = new URL(request.url).pathname;
 
-  switch (path) {
-    case '/status/mygames':
-      const myGamesStats = await STATS.get('myGames-latest');
-      if (!myGamesStats) {
-        return sendJSONResponse({ err: 'No Stats Data Found', contact: '@crashdoom' }, 500);
-      }
+  try {
+    switch (path) {
+      case '/status/mygames':
+        const myGamesStats = await STATS.get('myGames-latest');
+        if (!myGamesStats) {
+          return sendJSONResponse({ err: 'No Stats Data Found', contact: '@crashdoom' }, 500);
+        }
 
-      return sendJSONResponse(JSON.parse(myGamesStats), 200, true);
+        return sendJSONResponse({
+          ...JSON.parse(myGamesStats),
+          // Temp PSA re stats server being down
+          psa: 'Due to the My.Games Conqueror\'s Blade stats server being offline, updates for EU, NA, and RU cannot be polled at this time.',
+        }, 200, true);
 
-    case '/status/boominggames':
-      const boomingGamesStats = await STATS.get('boomingGames-latest');
-      if (!boomingGamesStats) {
-        return sendJSONResponse({ err: 'No Stats Data Found', contact: '@crashdoom' }, 500);
-      }
+      case '/status/boominggames':
+        const boomingGamesStats = await STATS.get('boomingGames-latest');
+        if (!boomingGamesStats) {
+          return sendJSONResponse({ err: 'No Stats Data Found', contact: '@crashdoom' }, 500);
+        }
 
-      return sendJSONResponse(JSON.parse(boomingGamesStats), 200, true);
+        return sendJSONResponse(JSON.parse(boomingGamesStats), 200, true);
 
-    case '/internal/mygames':
-      return updateMyGames(request);
+      case '/internal/mygames':
+        return updateMyGames(request);
 
-    case '/internal/booming':
-      return updateBoomingGames(request);
+      case '/internal/booming':
+        return updateBoomingGames(request);
+    }
+  } catch (e) {
+    return sendJSONResponse({ err: 'error' }, 500);
   }
 
   return sendJSONResponse({ err: 'missing route' }, 400);
@@ -107,11 +121,14 @@ function sendJSONResponse(json, code, cache = false) {
 }
 
 function addComparisonData(previousStats, newStats) {
-  if (!previousStats) {
+  if (!previousStats || !newStats) {
     return;
   }
 
   for (const key of Object.keys(newStats.servers)) {
+    if (!newStats.servers[key].online || !newStats.servers[key].data) {
+      continue;
+    }
     newStats.servers[key].data.diff = newStats.servers[key].data.current_user_count - previousStats.servers[key].data.current_user_count;
   }
 }
